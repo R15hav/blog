5f89a3516b7fb350f9ea9fe81b206f330b0693f5
R15hav <kumarrishav432@gmail.com>
2025-12-12 17:39:35 +0530

    base

--- app/app.py
+++ app/app.py
@@ -1,7 +1,31 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from app.schemas import PostBase
 
 app = FastAPI()
 
 @app.get("/get-blogs")
 def get_blogs():
     return {"message": "List of blogs"}
+
+@app.get("/get-blog/{id}")
+def get_blog(id: int):
+    return {"message": "Blog "+ str(id)}
+
+@app.post("/create-blog")
+def create_blog(post: PostBase):
+    return {"message": post}
+
+@app.put("/update-blog/{id}")
+def update_blog(id: int):
+    return {"message": "Blog "+ str(id) + " updated"}
+
+@app.delete("/delete-blog/{id}")
+def delete_blog(id: int):
+    return {"message": "Blog "+ str(id) + " deleted"}   
+
+@app.get("/search")
+def search(q: str = None):
+    if q:
+        return {"message": f"Search results for query: {q}"}
+    else:
+        raise HTTPException(status_code=400, detail="Query parameter 'q' is required")
